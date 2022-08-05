f52007ef7b6f22b313d8eac1f5ddae323838245c
Carlos Vinicius <carlos09v@gmail.com>
2022-08-05 06:26:52 -0300

    Contador_CodigoDev

---
+++ Web/Programadores/CodigoDev/Incrementador/script.js
@@ -0,0 +1,30 @@
+// CONTADOR INICIAL
+var count = 0
+
+// SELECIONA O VALOR DO BOTÃO
+const value = document.getElementById('value')
+const btns = document.querySelectorAll('.btn')
+
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList
+        if (styles.contains('subtrai')) {
+            count --
+        }else if (styles.contains('soma')){
+            count ++
+        }else {
+            count = 0
+        }
+
+        if (count > 0) {
+            value.style.color = '#34A853'
+        }else if (count < 0) {
+            value.style.color = '#EA4335'
+        }else {
+            value.style.color = '#333'
+        }
+
+        value.textContent = count
+    })
+})
