61b57d64bdad020f7d8ef74d85f4e1df2e3dd388
Carlos Vinicius <carlos09v@gmail.com>
2022-10-16 16:44:44 -0300

    hack_mode+labelFix+Rebirth1+imgs
    Update style.css
    
    imgs

--- Web/Eu/FruitClicker/assets/a.js
+++ Web/Eu/FruitClicker/assets/a.js
@@ -2,11 +2,15 @@ const level = document.getElementById('level'),
 visor = document.getElementById('visor'),
 frutatxt = document.getElementById('frutatxt'),
 img = document.getElementById('fruit1'),
-btn = document.getElementsByClassName('btn')
+btn = document.getElementsByClassName('btn'),
+rebirth = document.querySelector('header h3'),
+rebirValue = document.querySelector('#rebirth-value'),
+body = document.querySelector('body')
 
 
 let count = 0
 let countlv = 1
+let reb = 0
 
 
 img.addEventListener('click', e => {
@@ -15,40 +19,73 @@ img.addEventListener('click', e => {
   frutatxt.innerHTML = 'Continue Clicando ! 😀'
   visor.textContent = count
 
-  if (count >= 25) {
-    countlv + 1
+  if (count == 25) {
+    countlv += 1
     level.innerText = countlv
     level.style.color = 'green'
-  }
-  
-  if (count >= 50) {
-    countlv + 1
+    img.setAttribute('src', './assets/pexels-cachoBanana1.jpg')
+    img.parentNode.style.padding = '2px'
+  }else if (count == 50) {
+    countlv += 1
     level.innerText = countlv
     level.style.color = 'blue'
-  }
-  
-  if (count >= 75) {
-    countlv + 1
+    img.setAttribute('src', './assets/pexels-cachoBanana2.jpg')
+  }else if(count == 75) {
+    countlv += 1
     level.innerText = countlv
     level.style.color = 'red'
     frutatxt.innerHTML = "FINAL BOSS !!"
-  }
-  
-  if (count == 100) {
-    countlv + 1
+    img.setAttribute('src', './assets/pexels-bananeira.jpg')
+  }else if(count == 100) {
+    countlv += 1
+    level.innerText = countlv
+    img.parentNode.style.display = 'none'
     level.style.color = 'purple'
     frutatxt.innerHTML = " 🎆 CONGRATULATIONS ! 🎆"
-  }else if (count > 100) {
-    reset()
+  
+    const makeRebirth = document.createElement('p')
+    makeRebirth.innerHTML = '⬇️ Faça o rebirth ⬇️'
+    makeRebirth.style.textAlign = 'center'
+    frutatxt.appendChild(makeRebirth)
+  
+    btn[0].style.display = 'none'
+    btn[1].style.display = 'block'
   }
 })
 
-
+function rebir() {
+  reb += 1
+  rebirValue.innerText = reb
+  rebirth.style.display = 'block'
+  body.style.backgroundColor = 'blueviolet'
+  img.parentNode.style.display = 'block'
+  btn[0].style.display = 'block'
+  btn[1].style.display = 'none'
+  
+  reset()
+}
 
 function reset() {
+  countlv = 0
   count = 0
-  visor.textContent = count
+  
+  level.innerText = countlv
+  visor.innerText = count
+  level.style.color = 'black'
   frutatxt.innerHTML = '⬇️ Clique na Banana ⬇️'
+  img.parentNode.style.padding = '20px'
+  img.setAttribute('src', './assets/banana.jpg')
+}
+
+function hack() {
+  countlv = 4
+  count = 99
+  level.innerText = countlv
+  level.style.color = 'red'
+  visor.innerText = count
+
+  img.parentNode.style.padding = '2px'
+  img.setAttribute('src', './assets/pexels-bananeira.jpg')
 }
 
 
